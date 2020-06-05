db7cf7ec22de008ade911d9cc4e818f65ab5aeba
ryanhanli <43120124+ryanhanli@users.noreply.github.com>
2020-06-05 01:09:53 -0400

    Create CopyScript.sh

---
+++ CopyScript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "🔥🔥🔥 Beginning build!! 🔥🔥🔥"
+firstline=$(head -n 1 source/changelog.md)
+read -a splitfirstline <<< $firstline
+version=${splitfirstline[1]}
+echo "You are building version" $version
+echo "Do you want to continue? (enter '1' for yes, '0' for no)"
+read versioncontinue
+if [ $versioncontinue -eq 1 ]
+then
+  echo "OK"
+  for filename in source/*
+  do
+    if [ $filename == "source/secretinfo.md" ]
+    then
+      echo $filename "is not being copied"
+    else
+      echo $filename "is being copied"
+      cp $filename build/.
+    fi
+    #echo $filename
+  done
+  cd build/
+  echo "Build version $version contains:"
+  ls
+  cd ..
+else
+  echo "Please come back when you are ready"
+fi
